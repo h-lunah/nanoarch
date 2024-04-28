2331ab93403333a9a8373420df35ebeac4492a00
Piotr Zalewski <bardzoglodnypiotrek@gmail.com>
2024-04-28 18:35:37 +0000

    deal with s6 packages, use systemd bootloader, save kernel space

--- archiso-s6/profiledef.sh
+++ archiso-s6/profiledef.sh
@@ -8,7 +8,7 @@ iso_application="NanoArch"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
---
+++ s6/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf *nanoarch-s6.db* *nanoarch-s6.files*
+repo-add nanoarch-s6.db.tar.gz *.tar.zst
