db2a5bae0509f47cb0c039a346d4363d738510dd
luna <71488561+piotr25691@users.noreply.github.com>
2024-05-01 17:18:20 +0200

    Must be executable

--- archiso-systemd/profiledef.sh
+++ archiso-systemd/profiledef.sh
@@ -15,5 +15,5 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
-  ["/etc/rc.local"]="0:0:400"
+  ["/etc/rc.local"]="0:0:755"
 )
